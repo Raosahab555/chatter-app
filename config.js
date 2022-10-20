8d96d72e11ddb4e5413c206df94dc19449bde9c3
Keshav-sandhu <116194642+Keshav-sandhu@users.noreply.github.com>
2022-10-20 10:39:26 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,15 @@
+const config = {
+    apiKey: "AIzaSyDksbX-p5E6HlMjttOq8xBYkO8uv70dgF8",
+    authDomain: "chatter-7bff8.firebaseapp.com",
+    databaseURL: 'https://console.firebase.google.com/project/chatter-7bff8/database/chatter-7bff8-default-rtdb/data/~2F',
+    projectId: "chatter-7bff8",
+    storageBucket: "chatter-7bff8.appspot.com",
+    messagingSenderId: "314297890485",
+    appId: "1:314297890485:web:b7ad21f9255362777430fc",
+    measurementId: "G-R1P7ECH7PC",
+    CometChatAppId: '222566279ab74547',
+    CometChatRegion: 'us',
+    CometChatAuthKey: '0116f6905d774c48ca95856b86fcee8deeb0659f',
+    CometChatAPIKey: 'eb60e63a41d322c0b171c07c819fb53eb04c1fa8',
+    CometChatWidgetId: '3d92dbaf-69a3-489b-a642-b60adec2bf84'
+};    
